41998e2869185dc272ac362f51b494bfbe10180c
Kane Stone <k.stone4@student.unimelb.edu.au>
2014-09-28 22:46:13 +1000

    update to Zenithpaths to remove redundant for loops and improve efficiency

--- Things_to_fix_before_publishable.md
+++ Things_to_fix_before_publishable.md
@@ -7,9 +7,11 @@
 3. Evening and morning measurements needs to be coded in properly.
 4. Needs netcdf format functionality, as well as original text format.
 5. Code in extra output - all layers, as well as Umkehr layers.
-6. Speed up nvaluezs. 
-7. Cloudy flag switch needs to be implemented.
-8. Potential for multiple scattering needs to be coded in.
-9. Potential for stray light needs to be coded in.
+6. Speed up nvaluezs. - Fixed: [more efficient code can be found here](https://bitbucket.org/kstone4/umkehr_algorithm/commits/c04cc59768e944cfbcfc8402cb52889dd72499a9)
+7. Try to speed up zenithpaths
+8. Cloudy flag switch needs to be implemented.
+9. Potential for multiple scattering needs to be coded in.
+10. Potential for stray light needs to be coded in.
+11. zenith paths gtan in iteration 2 may not be implemented properly, should be in phi2 as well as ds2.
 
 
--- Zenithpaths.m
+++ Zenithpaths.m
@@ -56,8 +56,8 @@ end
 
 atmos.Apparent = Apparent_Final;
 atmos.true_actual = True.actual;
-%figure;
-%plot(squeeze(zs(1,1,1,:)))
+figure;
+plot(squeeze(zs(1,1,1,:)))
 
 end
 
@@ -66,7 +66,9 @@ function [True Apparent_Initial Apparent_Final zs atmos] = ...
     Apparent,iscat,gamma,zs,iteration)                    
 %Calculates zenith paths when theta is greater than 90
 
-Rg(iscat) = atmos.Nr(i,iscat)*sind(Apparent(j));
+%Rg(iscat) = atmos.Nr(i,iscat)*sind(Apparent(j));
+Rg = ones(1,length(atmos.N(i,1:atmos.nlayers-1))).*...
+    atmos.Nr(i,iscat)*sind(Apparent(j)); 
 atmos.ztan = interp1(atmos.Nr(i,:),atmos.r,Rg(iscat),'linear','extrap');
 tanlayer = ceil(((atmos.ztan-atmos.r(1))/atmos.dz));
 
@@ -78,74 +80,88 @@ if tanlayer < 1
     return
 end    
 
-%just below the scattering point to just above the tangent layer.  
-for l = iscat-1:-1:tanlayer+1; 
-    a = atmos.r(l+1);
-    b = atmos.r(l);
-    x1 = ((1/atmos.N(i,l+1))*sqrt(atmos.N(i,l+1)^2*a^2-Rg(iscat)^2));
-    x2 = ((1/atmos.N(i,l))*sqrt(atmos.N(i,l)^2*b^2-Rg(iscat)^2));
-    dx = abs(x2-x1);
-    phi1 = (atmos.N(i,l+1)*Rg(iscat))/((atmos.N(i,l+1)^2)*(a^2)-...
-        (gamma(l+1)*(Rg(iscat)^2)));
-    phi2 = (atmos.N(i,l)*Rg(iscat))/((atmos.N(i,l)^2)*(b^2)-(gamma(l)*...
-        (Rg(iscat)^2)));  
-    
-    if iteration == 2
-        ds1 = ((atmos.N(i,l+1)^2)*(a^2))/((atmos.N(i,l+1)^2)*(a^2)-...
-            (gamma(l+1)*(Rg(iscat)^2)));
-        ds2 = ((atmos.N(i,l)^2)*(b^2))/((atmos.N(i,l)^2)*(b^2)-(gamma(l)*...
-            (Rg(iscat)^2)));
-        zs(i,j,iscat,l) = atmos.dz+dx*(ds1+ds2);
-    end
-        phi(l) = (dx*((phi1+phi2)/2))*(180/pi)*2;                         
-end    
-
-%tangent layer calculation here:
-l = tanlayer;
-a = atmos.r(l+1);
-b = atmos.ztan;
-x1 = ((1/atmos.N(i,l+1))*sqrt(atmos.N(i,l+1)^2*a^2-Rg(iscat)^2));
-x2 = 0;
+%just below the scattering point to just above the tangent layer. 
+b = atmos.r(tanlayer+1:iscat-1);
+a = b+atmos.dz;
+x2 = ((1./atmos.N(i,tanlayer+1:iscat-1)).*...
+    sqrt(atmos.N(i,tanlayer+1:iscat-1).^2.*b.^2-Rg(tanlayer+1:iscat-1).^2));
+x1 = ((1./atmos.N(i,tanlayer+2:iscat)).*...
+    sqrt(atmos.N(i,tanlayer+2:iscat).^2.*a.^2-Rg(tanlayer+2:iscat).^2));
 dx = abs(x2-x1);
-phi1 = (atmos.N(i,l)*Rg(iscat))/((atmos.N(i,l)^2)*(a^2)-(gamma(l)*...
-    (Rg(iscat)^2)));
-phi2 = (Rg(iscat)^2/atmos.ztan)/((Rg(iscat)^2/atmos.ztan^2)*(b^2)-...
-    (gamma(1)*(Rg(iscat)^2)));     
-phi(l) = (dx*((phi1+phi2)/2))*(180/pi)*2;  
+phi2 = (atmos.N(i,tanlayer+1:iscat-1).*Rg(tanlayer+1:iscat-1))./...
+    ((atmos.N(i,tanlayer+1:iscat-1).^2).*(b.^2)-...
+    (gamma(tanlayer+1:iscat-1).*Rg(tanlayer+1:iscat-1).^2));
+phi1 = (atmos.N(i,tanlayer+2:iscat).*Rg(tanlayer+2:iscat))./...
+    ((atmos.N(i,tanlayer+2:iscat).^2).*(a.^2)-...
+    (gamma(tanlayer+2:iscat).*Rg(tanlayer+2:iscat).^2));
 
 if iteration == 2
-    gtan = interp1(atmos.r,gamma,atmos.ztan,'linear','extrap');
-    ds1 = ((atmos.N(i,l+1)^2)*(a^2))/((atmos.N(i,l+1)^2)*(a^2)-(gamma(l+1)*...
-        Rg(iscat)^2));
-    ds2 = (((Rg(iscat)/atmos.ztan)^2)*(b^2))/(((Rg(iscat)/atmos.ztan)^2)*...
-        (b^2)-(gtan*Rg(iscat)^2));
-    zs(i,j,iscat,l) = atmos.dz+dx*(ds1+ds2);
+    
+    ds2 = ((atmos.N(i,tanlayer+1:iscat-1).^2).*(b.^2))./...
+        ((atmos.N(i,tanlayer+1:iscat-1).^2).*...
+        (b.^2)-(gamma(tanlayer+1:iscat-1).*(Rg(tanlayer+1:iscat-1).^2)));
+    ds1 = ((atmos.N(i,tanlayer+2:iscat).^2).*(a.^2))./...
+        ((atmos.N(i,tanlayer+2:iscat).^2).*...
+        (a.^2)-(gamma(tanlayer+2:iscat).*Rg(tanlayer+2:iscat).^2));       
+    zs(i,j,iscat,tanlayer+1:iscat-1) = atmos.dz+(dx.*(ds1+ds2));
+end
+phi_down = dx*((phi1+phi2)/2)'.*(180/pi)*2;
+
+%tangent layer calculation here:
+a = atmos.r(tanlayer+1);
+b = atmos.ztan;
+x1 = ((1/atmos.N(i,tanlayer+1))*sqrt(atmos.N(i,tanlayer+1)^2*a^2-Rg(tanlayer)^2));
+x2 = 0;
+dx = abs(x2-x1);
+
+if iteration == 1
+    gtan = gamma(1);
+    phi1 = (atmos.N(i,tanlayer)*Rg(tanlayer))/((atmos.N(i,tanlayer)^2)*(a^2)-(gamma(tanlayer)*...
+        (Rg(tanlayer)^2)));
+    phi2 = (Rg(tanlayer)^2/atmos.ztan)/((Rg(tanlayer)^2/atmos.ztan^2)*(b^2)-...
+        (gtan*(Rg(tanlayer)^2)));  
+    phi_tangent = (dx*((phi1+phi2)/2))*(180/pi)*2;  
+end
+if iteration == 2
+    gtan = interp1(atmos.r,gamma,atmos.ztan,'linear','extrap');    
+    ds1 = ((atmos.N(i,tanlayer+1).^2).*(a.^2))./...
+       ((atmos.N(i,tanlayer+1).^2).*...
+       (a.^2)-(gamma(tanlayer+1).*Rg(tanlayer+1).^2));  
+    ds2 = (((Rg(tanlayer)/atmos.ztan)^2)*(b^2))/...
+        (((Rg(tanlayer)/atmos.ztan)^2)*...
+         (b^2)-(gtan*Rg(tanlayer)^2));
+    zs(i,j,iscat,tanlayer) = atmos.dz+(dx.*(ds1+ds2));        
 end
 
 %calculation after tangent when light goes up
-for l = iscat:atmos.nlayers-1; 
-    a = atmos.r(l);
-    b = a+atmos.dz;
-    x1 = ((1/atmos.N(i,l))*sqrt(atmos.N(i,l)^2*a^2-Rg(iscat)^2));
-    x2 = ((1/atmos.N(i,l+1))*sqrt(atmos.N(i,l+1)^2*b^2-Rg(iscat)^2));
-    dx = abs(x2-x1);
-    phi1 = (atmos.N(i,l)*Rg(iscat))/((atmos.N(i,l)^2)*(a^2)-(gamma(l)*...
-        (Rg(iscat)^2)));
-    phi2 = (atmos.N(i,l+1)*Rg(iscat))/((atmos.N(i,l+1)^2)*(b^2)-(gamma(l+1)...
-        *(Rg(iscat)^2)));           
-    
-    if iteration == 2
-        ds1 = ((atmos.N(i,l)^2)*(a^2))/((atmos.N(i,l)^2)*(a^2)-(gamma(l)*...
-            (Rg(iscat)^2)));
-        ds2 = ((atmos.N(i,l+1)^2)*(b^2))/((atmos.N(i,l+1)^2)*(b^2)-(gamma(l+1)...
-            *(Rg(iscat)^2)));
-        zs(i,j,iscat,l) = dx*(ds1+ds2)/2;
-    end    
-    phi(l) = (dx*((phi1+phi2)/2))*(180/pi);                               
-end    
+a = atmos.r(iscat:atmos.nlayers-1);
+b = a+atmos.dz;
+x1 = ((1./atmos.N(i,iscat:atmos.nlayers-1)).*...
+    sqrt(atmos.N(i,iscat:atmos.nlayers-1).^2.*a.^2-Rg(iscat:atmos.nlayers-1).^2));
+x2 = ((1./atmos.N(i,iscat+1:atmos.nlayers)).*...
+    sqrt(atmos.N(i,iscat+1:atmos.nlayers).^2.*b.^2-Rg(iscat:atmos.nlayers-1).^2));
+dx = abs(x2-x1);
+phi1 = (atmos.N(i,iscat:atmos.nlayers-1).*Rg(iscat:atmos.nlayers-1))./...
+    ((atmos.N(i,iscat:atmos.nlayers-1).^2).*(a.^2)-...
+    (gamma(iscat:atmos.nlayers-1).*Rg(iscat:atmos.nlayers-1).^2));
+phi2 = (atmos.N(i,iscat+1:atmos.nlayers).*Rg(iscat:atmos.nlayers-1))./...
+    ((atmos.N(i,iscat+1:atmos.nlayers).^2).*(b.^2)-...
+    (gamma(iscat+1:atmos.nlayers).*Rg(iscat:atmos.nlayers-1).^2));
+
+phi_up = dx*((phi1+phi2)./2)'.*(180/pi);
+
+if iteration == 2
+    ds1 = ((atmos.N(i,iscat:atmos.nlayers-1).^2).*(a.^2))./...
+        ((atmos.N(i,iscat:atmos.nlayers-1).^2).*...
+        (a.^2)-(gamma(iscat:atmos.nlayers-1).*(Rg(iscat:atmos.nlayers-1).^2)));
+    ds2 = ((atmos.N(i,iscat+1:atmos.nlayers).^2).*(b.^2))./...
+        ((atmos.N(i,iscat+1:atmos.nlayers).^2).*...
+        (b.^2)-(gamma(iscat+1:atmos.nlayers).*(Rg(iscat:atmos.nlayers-1).^2)));       
+    zs(i,j,iscat,iscat:atmos.nlayers-1) = dx.*(ds1+ds2)/2;
+end
 
 if iteration == 1
-    phi_total = sum(phi);
+    phi_total = phi_down+phi_up+phi_tangent;
     AngleTOA = asind(Rg(iscat)/atmos.Nr(i,end));
     Apparent_Initial(i,iscat,j) = Apparent(j);
     True.Initial(i,iscat,j) = AngleTOA+phi_total;
@@ -158,53 +174,40 @@ function [True Apparent_Initial Apparent_Final zs] = ...
     zenithpaths_down(atmos,i,j,True,Apparent_Initial,Apparent_Final,...
     Apparent,iscat,gamma,zs,iteration)
 %Calculates down through the atmosphere for cases where theta is less than 90           
-Rg(iscat) = atmos.Nr(i,iscat)*sind(Apparent(j)); 
+Rg = ones(1,length(atmos.N(i,iscat:atmos.nlayers-1))).*...
+    atmos.Nr(i,iscat)*sind(Apparent(j)); 
 
 %layers above the scattering height which are the slant paths  
-for l = iscat:atmos.nlayers-1; 
-    a = atmos.r(l);
-    b = a+atmos.dz;
-    x1 = ((1/atmos.N(i,l))*sqrt(atmos.N(i,l)^2*a^2-Rg(iscat)^2));
-    %x1_test(l) = ((1/atmos.N(i,l))*sqrt(atmos.N(i,l)^2*a^2-Rg(iscat)^2)); %for testing
-    x2 = ((1/atmos.N(i,l+1))*sqrt(atmos.N(i,l+1)^2*b^2-Rg(iscat)^2));
-    %x2_test(l) = ((1/atmos.N(i,l+1))*sqrt(atmos.N(i,l+1)^2*b^2-Rg(iscat)^2)); %for testing
-    dx = abs(x2-x1);
-    %dx1(l) = abs(x2-x1); %for testing
-    phi1 = (atmos.N(i,l)*Rg(iscat))/((atmos.N(i,l)^2)*(a^2)-(gamma(l)*...
-        (Rg(iscat)^2)));
-    phi2 = (atmos.N(i,l+1)*Rg(iscat))/((atmos.N(i,l+1)^2)*(b^2)-...
-        (gamma(l+1)*(Rg(iscat)^2)));  
-    
-    if iteration == 2
-        ds1 = ((atmos.N(i,l)^2)*(a^2))/((atmos.N(i,l)^2)*(a^2)-(gamma(l)*...
-            (Rg(iscat)^2)));
-       % ds1_test(l) = ((atmos.N(i,l)^2)*(a^2))/((atmos.N(i,l)^2)*(a^2)-(gamma(l)*...
-       %     (Rg(iscat)^2)));
-        ds2 = ((atmos.N(i,l+1)^2)*(b^2))/((atmos.N(i,l+1)^2)*(b^2)-...
-            (gamma(l+1)*(Rg(iscat)^2)));        
-        %ds2_test(l) = ((atmos.N(i,l+1)^2)*(b^2))/((atmos.N(i,l+1)^2)*(b^2)-...
-        %    (gamma(l+1)*(Rg(iscat)^2)));
-        
-        %ds1_temp(l) = 1/(1+(gamma(l).*(sind(Apparent(j).^2))));
-        %ds2_temp(l) = 1/(1+(gamma(l+1).*(sind(Apparent(j).^2))));
-        
-        zs(i,j,iscat,l) = dx*(ds1+ds2)/2;
-    end
-    
-    if iteration == 1
-        if l == iscat                                        
-            phi(l) = (dx*((phi1+phi2)/2))*(180/pi);               
-        else
-             phi(l) = phi(l-1)+((dx*((phi1+phi2)/2))*(180/pi));
-        end
-    end
+a = atmos.r(iscat:atmos.nlayers-1);
+b = a+atmos.dz;
+x1 = ((1./atmos.N(i,iscat:atmos.nlayers-1)).*...
+    sqrt(atmos.N(i,iscat:atmos.nlayers-1).^2.*a.^2-Rg.^2));
+x2 = ((1./atmos.N(i,iscat+1:atmos.nlayers)).*...
+    sqrt(atmos.N(i,iscat+1:atmos.nlayers).^2.*b.^2-Rg.^2));
+dx = abs(x2-x1);
+phi1 = (atmos.N(i,iscat:atmos.nlayers-1).*Rg)./...
+    ((atmos.N(i,iscat:atmos.nlayers-1).^2).*(a.^2)-...
+    (gamma(iscat:atmos.nlayers-1).*Rg.^2));
+phi2 = (atmos.N(i,iscat+1:atmos.nlayers).*Rg)./...
+    ((atmos.N(i,iscat+1:atmos.nlayers).^2).*(b.^2)-...
+    (gamma(iscat+1:atmos.nlayers).*Rg.^2));
 
+phi = dx*((phi1+phi2)./2)'.*(180/pi);
+
+if iteration == 2
+    ds1 = ((atmos.N(i,iscat:atmos.nlayers-1).^2).*(a.^2))./...
+        ((atmos.N(i,iscat:atmos.nlayers-1).^2).*...
+        (a.^2)-(gamma(iscat:atmos.nlayers-1).*(Rg.^2)));
+    ds2 = ((atmos.N(i,iscat+1:atmos.nlayers).^2).*(b.^2))./...
+        ((atmos.N(i,iscat+1:atmos.nlayers).^2).*...
+        (b.^2)-(gamma(iscat+1:atmos.nlayers).*(Rg.^2)));       
+    zs(i,j,iscat,iscat:atmos.nlayers-1) = dx.*(ds1+ds2)/2;
 end
 
 if iteration == 1
-    AngleTOA = asind(Rg(iscat)/atmos.Nr(i,end));
+    AngleTOA = asind(Rg(1)/atmos.Nr(i,end));
     Apparent_Initial(i,iscat,j) = Apparent(j);
-    True.Initial(i,iscat,j) = AngleTOA+phi(end);
+    True.Initial(i,iscat,j) = AngleTOA+phi;
 else
     Apparent_Final(i,iscat,j) = Apparent(j);
 end    
